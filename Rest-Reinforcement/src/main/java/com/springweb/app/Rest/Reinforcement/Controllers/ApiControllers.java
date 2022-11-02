fc0efb65f670821dd636fcf9f228c3c7640b222a
tebarker <tenaebarker@gmail.com>
2022-11-02 16:35:20 -0400

    Persistence has been created. application.properties has been updated

--- Rest-Reinforcement/src/main/java/com/springweb/app/Rest/Reinforcement/Controllers/ApiControllers.java
+++ Rest-Reinforcement/src/main/java/com/springweb/app/Rest/Reinforcement/Controllers/ApiControllers.java
@@ -1,12 +1,25 @@
 package com.springweb.app.Rest.Reinforcement.Controllers;
 
+import com.springweb.app.Rest.Reinforcement.Models.User;
+import com.springweb.app.Rest.Reinforcement.Repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
 public class ApiControllers {
+
+    @Autowired
+    private UserRepo userRepo;
     @GetMapping(value = "/")
     public String getPage() {
         return "Welcome";
+    }
+    public List<User> getUsers(){
+        return userRepo.findAll();
+
     }
 }
--- Rest-Reinforcement/src/main/java/com/springweb/app/Rest/Reinforcement/Repos/UserRepo.java
+++ Rest-Reinforcement/src/main/java/com/springweb/app/Rest/Reinforcement/Repos/UserRepo.java
@@ -3,6 +3,6 @@ package com.springweb.app.Rest.Reinforcement.Repos;
 import com.springweb.app.Rest.Reinforcement.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//public class UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository <User, Long> {
 
-//}
+}

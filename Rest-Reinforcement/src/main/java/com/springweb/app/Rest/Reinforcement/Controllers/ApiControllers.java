a22146c2ffe76fcf5dae7990fa9a0901c03e0f16
tebarker <tenaebarker@gmail.com>
2022-11-08 13:40:14 -0500

    Using @PutMapping to allow me to update user data

--- Rest-Reinforcement/src/main/java/com/springweb/app/Rest/Reinforcement/Controllers/ApiControllers.java
+++ Rest-Reinforcement/src/main/java/com/springweb/app/Rest/Reinforcement/Controllers/ApiControllers.java
@@ -3,10 +3,7 @@ package com.springweb.app.Rest.Reinforcement.Controllers;
 import com.springweb.app.Rest.Reinforcement.Models.User;
 import com.springweb.app.Rest.Reinforcement.Repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,8 @@ public class ApiControllers {
         return "Welcome";
     }
     @PostMapping(value = "/save")
-    public String saveUser(@RequestBody User user){
+    public String saveUser(@RequestBody User user){ //@RequestBody annotation maps the HttpRequest body to a transfer or domain object,
+        // enabling automatic deserialization of the inbound HttpRequest body onto a Java object.
 
         //request body Json
         userRepo.save(user); //pass in new user and save user to the database
@@ -32,5 +30,10 @@ public class ApiControllers {
     public List<User> getUsers(){//list of users
         return userRepo.findAll();//returns all users from the database.
 
+    }
+
+    @PutMapping (value = "update/{id}")
+    public String updateUser(@PathVariable int id, @RequestBody User user){
+        
     }
 }
